4cf4e901537f62953e46573fd4f5efd187130718
rdiaz71 <rndiaz57@gmail.com>
2023-08-02 15:54:33 -0500

    modified:   car.py modified:   car_factory.py new file:   test/test_tires/__init__.py new file:   test/test_tires/test_carrigan_tires.py new file:   test/test_tires/test_octoprime_tires.py new file:   tires/__init__.py new file:   tires/carrigan_tires.py new file:   tires/octoprime_tires.py new file:   tires/tires.py

--- car.py
+++ car.py
@@ -1,10 +1,11 @@
 from serviceable import Serviceable
 
 class Car(Serviceable):
-    def __init__(self, Engine, Battery):
+    def __init__(self, Engine, Battery, Tires):
         self.engine = Engine
         self.battery = Battery
+        self.tires = Tires
         
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
         
