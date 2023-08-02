388ae85d47c3319844d706bf03568ca60c94df6f
rdiaz71 <105741931+rdiaz71@users.noreply.github.com>
2023-08-01 21:50:55 -0500

    Add files via upload
    Task 2 refactor, add batteries

---
+++ battery/spindler_battery.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+from battery import Battery
+
+
+class SpindlerBattery(Battery, ABC):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage > 30000
