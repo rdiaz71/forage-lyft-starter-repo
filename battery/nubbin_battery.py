31877fba38999fca11472ede49a8d4b44c4f017f
rdiaz71 <105741931+rdiaz71@users.noreply.github.com>
2023-08-02 14:22:05 -0500

    Add files via upload
    Task 3 update

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,12 +1,15 @@
-from abc import ABC
+from battery.battery import Battery
+from utils import add_years_to_date
 
-from battery import Battery
 
-
-class NubbinBattery(Battery, ABC):
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
         self.current_date = current_date
+        self.last_service_date = last_service_date
 
     def needs_service(self):
-        return self.current_mileage - self.last_service_mileage > 30000
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
+        if date_which_battery_should_be_serviced_by < self.current_date:
+            return True
+        else:
+            return False
