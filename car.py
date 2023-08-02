2d2ee7030e007b690be06de4ac4a7043432af06f
rdiaz71 <105741931+rdiaz71@users.noreply.github.com>
2023-08-01 21:46:12 -0500

    Update car.py
    Task 2 refactor

--- car.py
+++ car.py
@@ -1,10 +1,13 @@
 from abc import ABC, abstractmethod
-
+from engine import Engine
+from battery import Battery
 
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
+    def __init__(self, Engine, Battery):
+        self.engine = Engine
+        self.battery = Battery
+        
     @abstractmethod
     def needs_service(self):
-        pass
+        return self.engine.needs_service or self.battery.needs_service
+        
